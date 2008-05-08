8b1e1d13a876c8064f80afe11e8997a45a05aa25
Brendon <425026+brendon-codes@users.noreply.github.com>
2008-05-08 21:27:19 +0000

    * Fixing licensing texts
    git-svn-id: http://drupy.svn.sourceforge.net/svnroot/drupy@149 e99eefd2-4c48-0410-9653-ee46b6e409b8

--- docs/PORTED-README.txt
+++ docs/PORTED-README.txt
@@ -1,3 +1,11 @@
+##
+## About This Document
+##
+
+  author Brendon Brendonopyright 2008 Brendon Brendonontact Brendon at users dot sourceforge dot net
+  created 2008-01-10
+  version 0.1
+
 Anytime a file is ported from Drupal CVS HEAD to Drupy, a copy of file should
 be stored in this directory "drupal-ported". This provides an easy way to track changes
 with newer CVS revisions.
--- trunk/includes/menu.py
+++ trunk/includes/menu.py
@@ -1,9 +1,36 @@
-#execfile('../lib/drupy/DrupyPHP.py')
-#
 # Id: menu.inc,v 1.267 2008/03/21 08:32:24 dries Exp $
+
 #
-# @file
-# API for the Drupal menu system.
+# @package Drupy
+# @see http://drupy.net
+# @note Drupy is a port of the Drupal project.
+#  The Drupal project can be found at http://drupal.org
+# @file menu.py (ported from Drupal's menu.inc)
+#  API for the Drupal menu system.
+# @author Morphir
+# @copyright 2008 Morphir
+# @contact morphir at users dot sourceforge dot net
+# @created 2008-01-10
+# @version 0.1
+# @license: 
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+
+
 #
 #
 # @defgroup menu Menu system
