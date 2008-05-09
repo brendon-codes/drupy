244916834e2daef3bf474776a54fede1ba6dd38d
Brendon <425026+brendon-codes@users.noreply.github.com>
2008-05-08 19:36:08 -0700

    * Trying again

--- scripts/ciabot.bash
+++ scripts/ciabot.bash
@@ -1,17 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Distributed under the terms of the GNU General Public License v2
 # Copyright (c) 2006 Fernando J. Pereda <ferdy@gentoo.org>
-# Copyright (c) 2008 Leonardo Valeri Manera <l DOT valerimanera @ gmail DOT com>
 #
 # Git CIA bot in bash. (no, not the POSIX shell, bash).
 # It is *heavily* based on Git ciabot.pl by Petr Baudis.
 #
-# It is meant to be run in an update hook:
+# It is meant to be run either on a post-commit hook or in an update
+# hook:
 #
-#   refname=$1
-#   oldhead=$2
-#   newhead=$3
-#   /path/to/ciabot.bash ${refname} ${oldhead} ${newhead} gitweb/project/name.git &
+# post-commit: It parses latest commit and current HEAD to get the
+# information it needs.
+#
+# update: You have to call it once per merged commit:
+#
+#       refname=$1
+#       oldhead=$2
+#       newhead=$3
+#       for merged in $(git rev-list ${oldhead}..${newhead} | tac) ; do
+#               /path/to/ciabot.bash ${refname} ${merged}
+#       done
 #
 
 # The project as known to CIA
@@ -21,44 +28,42 @@ project="drupy"
 noisy=true
 
 # Addresses for the e-mail
-to="cia@cia.vc"
+from="you@yourthing.com"
+to="cia@cia.navi.cx"
 
 # SMTP client to use
-sendmail="/usr/sbin/sendmail ${to} -i"
-
-# gitweb project
-gitweb_project="${4:-${project}.git}"
+sendmail="/usr/sbin/sendmail -f ${from}"
 
 # Changeset URL
-url="http://git.einit.org/?p=${gitweb_project};a=commit;h=@@sha1@@"
+url="http://www.example.com/gitweb/?p=${project}.git;a=commit;h=@@sha1@@"
 
-# The script itself
+# You shouldn't be touching anything else.
+if [[ $# = 0 ]] ; then
+	refname=$(git symbolic-ref HEAD 2>/dev/null)
+	merged=$(git rev-parse HEAD)
+else
+	refname=$1
+	merged=$2
+fi
+
+refname=${refname##refs/heads/}
 
 gitver=$(git --version)
 gitver=${gitver##* }
-
-oldhead=$2
-newhead=$3
-
-refname=${1##refs/heads/}
-
-mail_cia() {
 
 rev=$(git describe ${merged} 2>/dev/null)
 [[ -z ${rev} ]] && rev=${merged:0:12}
 
 rawcommit=$(git cat-file commit ${merged})
 
-author=$(grep author <<< "$rawcommit" | sed -e 's:author \(.*\) <.*:\1:')
+author=$(sed -n -e '/^author .*<\([^@]*\).*$/s--\1-p' \
+	<<< "${rawcommit}")
 
 logmessage=$(sed -e '1,/^$/d' <<< "${rawcommit}")
 ${noisy} || logmessage=$(head -n 1 <<< "${logmessage}")
 logmessage=${logmessage//&/&amp;}
 logmessage=${logmessage//</&lt;}
 logmessage=${logmessage//>/&gt;}
-
-repository=$(echo "${gitweb_project}" | sed -e 's:.*/::g' -e 's:\.git::')
-logmessage=":${repository}: ${logmessage}"
 
 ts=$(sed -n -e '/^author .*> \([0-9]\+\).*$/s--\1-p' \
 	<<< "${rawcommit}")
@@ -68,7 +73,7 @@ out="
   <generator>
     <name>CIA Bash client for Git</name>
     <version>${gitver}</version>
-    <url>http://jyujin.de/~creidiki/ciabot.bash</url>
+    <url>http://dev.gentoo.org/~ferdy/stuff/ciabot.bash</url>
   </generator>
   <source>
     <project>${project}</project>
@@ -99,11 +104,5 @@ Content-type: text/xml
 Subject: DeliverXML
 ${out}
 EOM
-
-}
-
-for merged in $(git rev-list ${oldhead}..${newhead} | tac); do
-    mail_cia
-done
 
 # vim: set tw=70 :
