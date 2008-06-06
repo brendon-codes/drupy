6cf6def9a859ee728bf1aa15d0b90b2175afa7e8
Brendon <425026+brendon-codes@users.noreply.github.com>
2008-06-05 18:00:34 -0700

    * Fixed webtest file

--- base/webtest.py
+++ base/webtest.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python
 
-import os
-from beaker import session
-
-sess = session.SessionObject(os.environ, type='file', data_dir='/tmp/cookies')._session()
-
-if not sess.has_key('foo'):
-  sess['foo'] = 1
-else:
-  sess['foo'] += 1
-sess.save()
-
-print sess.cookie
-print "Content-Type: text/plain\r\n\r\n"
+print "Content-Type: text/plain; Charset=UTF-8;\r\n\r\n"
 print "Page Loaded."
-print sess
