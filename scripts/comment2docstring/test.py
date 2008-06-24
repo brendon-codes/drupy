193e8fc773be103e71bd85b650e225914cd79a08
Brendon <425026+brendon-codes@users.noreply.github.com>
2008-06-23 17:16:10 -0700

    * Improved support for comment2docstring tokenizer * Converted docstrings for files

---
+++ scripts/comment2docstring/test.py
@@ -0,0 +1,22 @@
+
+# Dont do anything here
+BAZZZ = 1
+
+#
+# Dont do anything here either
+#
+
+
+
+#
+# Hi this is a comment 1
+# This is another comment 2
+#
+
+
+
+def foobar(baz):
+    doSomething()
+    return
+
+
