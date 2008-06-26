5c8876a190aa8406d40d105559d0f6d746536f62
Brendon <425026+brendon-codes@users.noreply.github.com>
2008-06-26 01:47:34 -0700

    * Converted docstrings

--- base/index.py
+++ base/index.py
@@ -2,38 +2,38 @@
 
 # $Id: index.php,v 1.94 2007/12/26 08:46:48 dries Exp $
 
-#
-# @package Drupy
-# @see http://drupy.net
-# @note Drupy is a port of the Drupal project.
-#  The Drupal project can be found at http://drupal.org
-# @file index.py (ported from Drupal's index.php)
-#  The Python page that serves all page requests on a Drupy installation.
-#  The routines here dispatch control to the appropriate handler, which then
-#  prints the appropriate page.
-#  All Drupy code is released under the GNU General Public License.
-# @see COPYRIGHT.txt, LICENSE.txt.
-# @author Brendon Crawford
-# @copyright 2008 Brendon Crawford
-# @contact message144 at users dot sourceforge dot net
-# @created 2008-01-10
-# @version 0.1
-# @license: 
-#
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
+"""
+ @package Drupy
+ @see http://drupy.net
+ @note Drupy is a port of the Drupal project.
+  The Drupal project can be found at http://drupal.org
+ @file index.py (ported from Drupal's index.php)
+  The Python page that serves all page requests on a Drupy installation.
+  The routines here dispatch control to the appropriate handler, which then
+  prints the appropriate page.
+  All Drupy code is released under the GNU General Public License.
+ @see COPYRIGHT.txt, LICENSE.txt.
+ @author Brendon Crawford
+ @copyright 2008 Brendon Crawford
+ @contact message144 at users dot sourceforge dot net
+ @created 2008-01-10
+ @version 0.1
+ @license: 
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
 
 
 from lib.drupy import DrupyPHP as p
